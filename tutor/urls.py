22d8cc653ce046bc51113ca4b8f9317c8a2edb8a
laughingwb <308849014@qq.com>
2017-08-17 00:28:18 +0800

    教师界面
    教师界面

--- tutor/urls.py
+++ tutor/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views as tutor_view
 
 urlpatterns = [
-
+    url(r'^tutor_list/',tutor_view.tutor_list, name='tutor_list'),
 ]
--- tutor/views.py
+++ tutor/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def tutor_list(request):
+    return render(request, 'tutor/tutor_list.html')
